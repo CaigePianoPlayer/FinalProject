b28082935c9dcab0740a168fa1acf9ad9eb1458f
Caige.hazelwood <caige.hazelwood@stu.henderson.kyschools.us>
2016-05-12 13:53:34 -0500

    It works!!!! The Button Works!!!!!

--- app/src/main/java/com/example/caigehazelwood/hchsguidetogood/Map.java
+++ app/src/main/java/com/example/caigehazelwood/hchsguidetogood/Map.java
@@ -1,9 +1,12 @@
 package com.example.caigehazelwood.hchsguidetogood;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+
+import static com.example.caigehazelwood.hchsguidetogood.R.id.button12;
 
 /**
  * Created by jewel.stone on 5/10/2016.
@@ -16,7 +19,8 @@ public class Map extends MainActivity {
             super.onCreate(savedInstanceState);
             setContentView(R.layout.map);
 
-            Button next1 = (Button) findViewById(R.id.button7);
+            Button next1 = (Button) findViewById(button12);
+            assert next1 != null;
             next1.setOnClickListener(new View.OnClickListener() {
                 public void onClick(View view) {
                     Intent intent = new Intent();
